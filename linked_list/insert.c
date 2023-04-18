c7b184cff6ed415bac7570129e5bc36156201a29
sommrook-42maru <sommrook@gmail.com>
2023-04-18 15:33:27 +0900

    insert linked list

--- linked_list/insert.c
+++ linked_list/insert.c
@@ -1,47 +1,79 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+// 그림 그리면서 하면 구현하기 쉬워짐!
+
 typedef struct NODE {
     int data;
     struct NODE* next;
 }node;
 
-void insert(int idex, int data)
+node* head;
+
+void Insert(int index, int data);
+
+void Print();
+
+void Insert(int index, int data)
 {
-    
+    node* new_node = (node*)malloc(sizeof(node));
+
+    new_node -> next = NULL;
+    new_node -> data = data;
+
+    if (head == NULL){
+        head = new_node;
+        return;
+    }
+    else{
+        node* temp = head;
+        int num = 0;
+        while (index >= num){
+            if (index == 0){
+                new_node -> next = temp;
+                head = new_node;
+                break;
+                return;
+            }
+            else{
+                if (index == num+1){
+                new_node -> next = temp -> next;
+                temp -> next = new_node;
+                break;
+                }
+            }
+            temp = temp -> next;
+            num += 1;
+
+        }
+    }
+    return;
+}
+
+void Print(){
+    node* curr = head;
+
+    while(curr -> next != NULL){
+        printf("%d\n", curr->data);
+        curr = curr->next;
+    } 
+    printf("%d\n", curr -> data);
 }
 
 int main()
 {
     // 헤드(시작)노드 생성
     // 메모리 동적 할당
-    node* head = (node*)malloc(sizeof(node));
+    head = NULL;
 
-    head -> next = NULL;
-
-    node* node1 = (node*)malloc(sizeof(node));
-    
-    head -> next = node1;
-    node1 -> data = 1;
-    node1 -> next = NULL;
-
-    node* node2 = (node*)malloc(sizeof(node));
-
-    node1 -> next = node2;
-    node2 -> data = 2;
-    node2 -> next = NULL;
-
-    node* curr = head->next;
-
-    while(curr != NULL){
-        printf("%d\n", curr->data);
-        curr = curr->next;
-    }
-
-    free(head);
-    free(node1);
-    free(node2);
-
-    return 0;
+    Insert(0, 6);
+    Insert(1, 7);
+    Insert(0, 7);
+    Insert(0, 8);
+    Insert(1, 20);
+    Insert(0, 10);
+    Insert(4, 50);
+    Insert(0, 20);
+    Print();
 
 }
